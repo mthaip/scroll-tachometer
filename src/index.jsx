3a0874a23f2a89a40d051ce86812d826c0665390
mthaip <fanmt.fmt@gmail.com>
2023-07-30 15:35:54 +0200

    feat: use scss

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import './index.scss';
 
-ReactDOM.createRoot(document.getElementById('app')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+ReactDOM.createRoot(document.getElementById('app')).render(<App />);
